5b68494e8f80747b7d712c4bb55a95b024155aa5
johanan <johanan2306@gmail.com>
2025-12-05 12:22:44 +0530

    updated Next.js for the Security Patch for CVE-2025-66478 vulnerability.

--- db/index.ts
+++ db/index.ts
@@ -1,8 +1,5 @@
 import { drizzle } from "drizzle-orm/neon-http";
 import { neon } from "@neondatabase/serverless";
-import { config } from "dotenv";
-
-config({ path: ".env" });
 
 const sql = neon(process.env.DATABASE_URL || "");
 
